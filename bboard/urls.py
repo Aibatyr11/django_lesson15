28bc64c3b1a95a425ec4064a51ae9eaf65265903
Aibatyr <aibatyr111@gmail.com>
2025-05-21 12:15:24 +0500

    hww

--- bboard/urls.py
+++ bboard/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import index, task_list_json
+from .views import index, task_list_json, add_task, task_list
 
 urlpatterns = [
     path('', index, name='index'),
     path('api/tasks/', task_list_json, name='task_list_json'),
+    path('add/', add_task, name='add_task'),
+    path('tasks/', task_list, name='task_list'),
 ]
--- bboard/views.py
+++ bboard/views.py
@@ -1,14 +1,34 @@
-from django.shortcuts import render
 from django.http import JsonResponse
 from .models import Task
+from django.shortcuts import render, redirect
+from django.http import Http404
+from .forms import TaskForm
+
 
 def index(request):
     return render(request, 'index.html')
 
 def task_list_json(request):
-    # List comprehension для формирования массива задач
     data = [
         {"id": task.id, "title": task.title, "completed": task.completed}
         for task in Task.objects.all()
     ]
     return JsonResponse(data, safe=False)
+
+
+
+def add_task(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/tasks/')
+        else:
+            raise Http404("Неверные данные!")
+
+    form = TaskForm()
+    return render(request, 'add_task.html', {'form': form})
+
+def task_list(request):
+    tasks = Task.objects.all()
+    return render(request, 'task_list.html', {'tasks': tasks})
